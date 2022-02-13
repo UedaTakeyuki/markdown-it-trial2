70e7d2c9c04c3c328b8849de2253baf18aa4121e
Takeyuki UEDA <ueda@latelierdueda.com>
2022-02-13 22:19:58 +0900

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <v-textarea
+      v-model="kero"
+    />
+    <v-card>
+      <div v-html="aho" />
+    </v-card>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Home',
+  data: ()=> ({
+    md: require('markdown-it')({
+  html: true,
+  linkify: true,
+  typographer: true
+}),
+    kero: ""
+  }),
+  computed: {
+    aho: function(){
+      return this.md.render(this.kero)
+    }
+  }
+}
+</script>
